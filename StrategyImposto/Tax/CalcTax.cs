2a367834ee18530969ba629564f07448b0974aec
Renan Zamonelli <47351828+RZamonelli@users.noreply.github.com>
2020-08-16 15:36:29 -0300

    First commit

---
+++ StrategyImposto/Tax/CalcTax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tax
+{
+    public class CalcTax
+    {
+        public double Calc(String tax, double value)
+        {
+            if (tax.Equals("ICMS"))
+                return value * 0.10;
+            else if (tax.Equals("IPI"))
+                return value * 0.20;
+            else if (tax.Equals("NEW_TAX"))
+                return value * 0.50;
+            return value;
+        }
+    }
+}
---
+++ StrategyImposto/UTCalcTax/UTTax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tax;
+
+namespace UTCalcTax
+{
+    [TestClass]
+    public class UTTax
+    {
+        [TestMethod]
+        public void TestCalcICMS()
+        {
+            double icms = new CalcTax().Calc("ICMS", 100);
+            Assert.AreEqual(10, icms);
+        }
+
+        [TestMethod]
+        public void TestCalcIPI()
+        {
+            double ipi = new CalcTax().Calc("IPI", 100);
+            Assert.AreEqual(20, ipi);
+        }
+
+        [TestMethod]
+        public void TestCalcNewTax()
+        {
+            double newTax = new CalcTax().Calc("NEW_TAX", 100);
+            Assert.AreEqual(50, newTax);
+        }
+    }
+}
